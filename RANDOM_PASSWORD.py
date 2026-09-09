dfeb3badc575d4fc0e2a77366442b10bff893f08
Bisgond Kiran <kiranbisgond@gmail.com>
2026-09-09 13:45:06 +0530

    Add password generation script

---
+++ RANDOM_PASSWORD.py
@@ -0,0 +1,7 @@
+import random
+characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*"
+length = int(input("Enter password length: "))
+password = ""
+for i in range(length):
+    password += random.choice(characters)
+print("Password:", password)
